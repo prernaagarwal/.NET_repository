ec216aeaf072b196a0e76b5caf1c3ad0a8d36b54
prernaagarwal <prerna214@gmail.com>
2018-07-20 18:50:04 +0530

    All asp controls have view state enabled by default

---
+++ WebSites/WebSite6/frm3.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="frm3.aspx.cs" Inherits="frm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="welcome" runat="server" Text="Welcome"></asp:Label><br /><br />
+        <%--<asp:GridView ID="grid" runat="server"></asp:GridView>--%><br /><br />
+        <asp:GridView ID="grid" runat="server" EnableViewState="False"></asp:GridView><br /><br />
+        <asp:Button ID="btn" runat="server" Text="Postback" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSites/WebSite6/frm3.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class frm3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            showGrid();
+        }
+    }
+
+    public void showGrid()
+    {
+        SqlConnection con = new SqlConnection(@"Data Source =DESKTOP-SEBSEGR;Initial Catalog=Employeedb;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand("select * from tblEmployee", con);
+        con.Open();
+        SqlDataReader sdr = cmd.ExecuteReader();
+        grid.DataSource = sdr;
+        grid.DataBind();
+        con.Close();
+    }
+}
