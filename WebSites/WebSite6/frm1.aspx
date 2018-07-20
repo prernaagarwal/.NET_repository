146f2bc9190fda051d068d40324ba91b0b9fa0c9
prernaagarwal <prerna214@gmail.com>
2018-07-20 16:53:51 +0530

    ViewState

---
+++ WebSites/WebSite6/frm1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="frm1.aspx.cs" Inherits="frm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lbl" runat="server"></asp:Label><br /><br />
+        <asp:Button ID ="btn1" runat="server" text ="submit" OnClick="btn1_Click"/> 
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebSites/WebSite6/frm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class frm1 : System.Web.UI.Page
+{
+    int count = 1;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+         if (ViewState["count"]!=null)
+        {
+            count = (int)ViewState["count"];
+        }
+
+        if (count == 1)
+        {
+            lbl.Text += "You have clicked me 1 time <br />";
+            
+        }
+        else if (count > 1 )
+        {
+            lbl.Text += "You have clicked me "+count+" times <br />";
+        }
+        count++;
+        ViewState["count"] = count;
+        
+    }
+}
